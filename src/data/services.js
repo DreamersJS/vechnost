cb388042ca89bbde7f405a2dd0238c1b1f3e59eb
Zvezda Neycheva <zvezda.neycheva.a55@learn.telerikacademy.com>
2026-06-04 01:15:10 +0300

    build failed trying diff next.js

---
+++ src/data/services.js
@@ -0,0 +1,32 @@
+export const services = [
+    {
+      title: "Погребални услуги",
+      href: "/pogrebalni-uslugi-sofia",
+      desc: "Организация на погребения в София",
+    },
+    {
+      title: "Кремации",
+      href: "/kremacii-sofia",
+      desc: "Кремации с пълна организация и документи",
+    },
+    {
+      title: "Транспорт на покойник",
+      href: "/transport-pokoinici",
+      desc: "България и чужбина",
+    },
+    {
+      title: "Траурни артикули",
+      href: "/traurni-artikuli",
+      desc: "Венци, цветя, ковчези и аксесоари",
+    },
+    {
+      title: "Каменоделски услуги",
+      href: "/kamenodelski-uslugi",
+      desc: "Облицовка, паметници и поддръжка",
+    },
+    {
+      title: "Почистване след смърт",
+      href: "/pochistvane-sled-smart",
+      desc: "Почистване на мазета и тавани и стройтелни дейности",
+    },
+  ];
